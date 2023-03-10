5516403f507496bcf185ff8d324364db44653e94
Jorge Monzon <jorge29luis10@gmail.com>
2023-03-10 12:46:20 -0500

    Agregando dirección a la navegación

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const botonBusqueda = document.querySelector('.botones__busqueda');
+const verIngresoBusqueda = document.querySelector('.site');
+
+// eventos
+botonBusqueda.addEventListener('click', () => {
+  verIngresoBusqueda.classList.toggle('showbusqueda');
+});
