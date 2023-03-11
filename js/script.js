8da2070a89f9156172bad15eb534797e46b17644
Jorge Monzon <jorge29luis10@gmail.com>
2023-03-11 00:12:43 -0500

    Agregar navegación en boton busqueda y boton menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-// const botonBusqueda = document.querySelector('.botones__busqueda');
-// const verIngresoBusqueda = document.querySelector('.site');
+const botonBusqueda = document.querySelector('.botones__busqueda');
+const verIngresoBusqueda = document.querySelector('.site');
 
-// // eventos
-// botonBusqueda.addEventListener('click', () => {
-//   verIngresoBusqueda.classList.toggle('showbusqueda');
-// });
+// eventos
+botonBusqueda.addEventListener('click', () => {
+  verIngresoBusqueda.classList.toggle('showbusqueda');
+});
+
+const botonMenu = document.querySelector('.botones__menu');
+const verIngresoMenu = document.querySelector('.site');
+
+botonMenu.addEventListener('click', () => {
+  // agregar clase al selectro que tiene la clase .site
+  verIngresoMenu.classList.toggle('showmenu');
+});
